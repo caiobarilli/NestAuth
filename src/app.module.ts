8df027921b52721546e946cf3f2566aed10aa8b4
Caio Barilli <caiobarilli@gmail.com>
2023-11-06 23:11:39 -0300

    feat: Remove AppController and update AppModule imports.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from '@config/typeorm.config';
-import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AuthModule],
-  controllers: [AppController],
-  providers: [],
 })
 export class AppModule {}
