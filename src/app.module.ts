1c8abb53a57efa0bef44460217d713ba757fd238
Caio Barilli <caiobarilli@gmail.com>
2023-11-10 05:26:03 -0300

    fix: path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from '@config/typeorm.config';
+import { typeOrmConfig } from './config/typeorm.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { IsUniqueConstraint } from './users/dto/custom-validators/data-unique.validator';
