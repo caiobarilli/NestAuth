30fa770f745f42f99459e12be9e1339ddd9eec5f
Caio Barilli <caiobarilli@gmail.com>
2023-11-11 02:45:21 -0300

    cleancode: new folder strategy in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [
