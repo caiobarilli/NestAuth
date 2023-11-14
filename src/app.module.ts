9c3685abea9a3b03fc8ca8229c3c5414aa97ee4d
Caio Barilli <caiobarilli@gmail.com>
2023-11-13 22:42:46 -0300

    feat: add winston logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { WinstonModule } from 'nest-winston';
 import { typeOrmConfig } from './config/typeorm.config';
+import { winstonConfig } from './config/winston.config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { IsUniqueConstraint } from './users/dto/custom-validators/data-unique.validator';
+import { LoggerInterceptor } from './interceptors /logger.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AuthModule],
-  providers: [IsUniqueConstraint],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    WinstonModule.forRoot(winstonConfig),
+    UsersModule,
+    AuthModule,
+  ],
+  providers: [
+    IsUniqueConstraint,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { winstonConfig } from './config/winston.config';
+import { WinstonModule } from 'nest-winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = WinstonModule.createLogger(winstonConfig);
+  const app = await NestFactory.create(AppModule, { logger });
   app.useGlobalPipes(new ValidationPipe());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(3000);
